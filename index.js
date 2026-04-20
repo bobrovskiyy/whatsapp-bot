3511ec05d2df08f86a10cc9d08dae59279703bd1
bobrovskiyy <ilyabobrovskiyy@gmail.com>
2026-04-20 20:07:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.post("/", async (req, res) => {
       const response = await axios.post(
         "https://openrouter.ai/api/v1/chat/completions",
         {
-          model: "openai/gpt-5-chat",
+          model: "google/gemini-3.1-pro-preview",
           messages: chat
         },
         {
